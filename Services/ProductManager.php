8d35936fdcd7442869fb6a7084111db00ce79ef7
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-05-14 23:22:33 +0700

    add file lab

---
+++ Services/ProductManager.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Services;
+
+class ProductManager
+{
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = [];
+    }
+
+    public function add($product) {
+        $this->products[] = $product;
+    }
+
+    public function getProducts() {
+        return $this->products;
+    }
+}
